d98c7f9b94a8bc2acf2162d80e5155829d46a40a
Juan Camba <juan_camba@yahoo.com.ar>
2023-09-22 20:08:20 +0200

    agregamos controller categorias

---
+++ BlogCore/Areas/Admin/Controllers/CategoriasController.cs
@@ -0,0 +1,39 @@
+﻿using BlogCore.AccesoDatos.Data.Repository.IRepository;
+using BlogCore.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriasController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ApplicationDbContext _dbContext;
+
+
+        public CategoriasController(IUnitOfWork unitOfWork, ApplicationDbContext dbContext)
+        {
+            _unitOfWork = unitOfWork;
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        #region
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+
+            return Json(new { Data = _unitOfWork._categoriaRepository.GetAll() });
+        }
+        #endregion
+
+    }
+}
